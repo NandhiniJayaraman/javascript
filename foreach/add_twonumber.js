c078dfce0f6724c33bb0a27aaac10e941853dd6c
NandhiniJayaraman <nansjayaraman@gmail.com>
2023-09-01 15:52:45 +0530

    foreach examples

---
+++ foreach/add_twonumber.js
@@ -0,0 +1,8 @@
+a=[2,3,4,5]
+b=[2,3,4,5]
+let sum1=0
+sum=a.forEach(function add(n){
+    sum1=sum1+n
+    console.log(sum1)
+})
+
---
+++ practices /cms.js
@@ -0,0 +1,11 @@
+function celsius(){
+
+    f=((n*(9/5)+32))
+    return f
+}
+// console.log(celsius(72))
+function farenheit(){
+    
+    c=((n*(5/9)+32))
+    return c
+}
